29a8d3ed2dd96d518f9dda26bb0da75478be29d0
Wacharaporn59 <59160130@go.buu.ac.th>
2019-11-18 17:47:59 +0700

    End

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,13 +1,15 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
-    // The final _score
+    // The final score
     var score = finalScore
     init {
-        Log.i("ScoreViewModel", "Final _score is $finalScore")
+        Log.i("ScoreViewModel", "Final score is $finalScore")
     }
 }
 
